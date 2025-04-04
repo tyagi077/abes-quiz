fe2518a16b91d175e020c1317c0dcd5bc3fbcdbf
Mayank Tyagi <tyagimayank0501@gmail.com>
2025-04-04 11:15:47 +0530

    added title and description

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -70,7 +70,7 @@ function App() {
   return (
     <div className="min-h-screen relative" >
       <div className="fixed top-0 w-full h-[300px] bg-[#66E1D7]">
-      <h2 className="text-red-500 font-semibold">*Under maintenance</h2>
+      <h2 className="text-red-500 font-semibold text-3xl">*Under maintenance</h2>
       </div>
       <div className="absolute py-4 flex flex-col gap-6  overflow-y-auto w-[90%] max-w-[400px]  bg-white px-4   shadow-2xl rounded-md top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40   ">
         <h3 className="text-gray-700 font-semibold text-lg text-center">Enter Your Details</h3>
