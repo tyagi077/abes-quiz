2e470294e9cde227ac154cf59820a23f03c83c8e
Mayank Tyagi <146551960+tyagi077@users.noreply.github.com>
2025-07-16 11:29:20 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
 
     try {
-      const response = await axios.post("http://localhost:3000/api/v1/fetch", {
+      const response = await axios.post("https://abes-quiz-backend.vercel.app/api/v1/fetch", {
         quiz_uc: formData.quiz_id,
         user_unique_code: formData.admission_number,
         pin: formData.pin
